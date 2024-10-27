903e28505822b5ab0bc69f20f5278e7da6487b7b
hugodiaz2 <hugomisael222@gmail.com>
2024-10-26 21:07:37 -0600

    Prueba
    Se hizo un espacio

--- src/main/java/test/example/springboot/test/demo/Model/Employee.java
+++ src/main/java/test/example/springboot/test/demo/Model/Employee.java
@@ -14,6 +14,7 @@ import lombok.*;
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
 
+        
         @Column(nullable = false)
         private String firstName;
 
